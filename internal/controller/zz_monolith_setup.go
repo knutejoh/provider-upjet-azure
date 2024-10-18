f2c3eb0edb6cc1cc18aaae277c72f3cba8a5fd35
Knut-Erik Johnsen <abstract@knut-erik.org>
2024-10-18 20:22:38 +0200

    Added mongodb role and userdefinitions
    Signed-off-by: Knut-Erik Johnsen <abstract@knut-erik.org>

--- internal/controller/zz_monolith_setup.go
+++ internal/controller/zz_monolith_setup.go
@@ -194,6 +194,8 @@ import (
 	gremlingraph "github.com/upbound/provider-azure/internal/controller/cosmosdb/gremlingraph"
 	mongocollection "github.com/upbound/provider-azure/internal/controller/cosmosdb/mongocollection"
 	mongodatabase "github.com/upbound/provider-azure/internal/controller/cosmosdb/mongodatabase"
+	mongoroledefinition "github.com/upbound/provider-azure/internal/controller/cosmosdb/mongoroledefinition"
+	mongouserdefinition "github.com/upbound/provider-azure/internal/controller/cosmosdb/mongouserdefinition"
 	sqlcontainer "github.com/upbound/provider-azure/internal/controller/cosmosdb/sqlcontainer"
 	sqldatabase "github.com/upbound/provider-azure/internal/controller/cosmosdb/sqldatabase"
 	sqldedicatedgateway "github.com/upbound/provider-azure/internal/controller/cosmosdb/sqldedicatedgateway"
@@ -938,6 +940,8 @@ func Setup_monolith(mgr ctrl.Manager, o controller.Options) error {
 		gremlingraph.Setup,
 		mongocollection.Setup,
 		mongodatabase.Setup,
+		mongoroledefinition.Setup,
+		mongouserdefinition.Setup,
 		sqlcontainer.Setup,
 		sqldatabase.Setup,
 		sqldedicatedgateway.Setup,
